1a9d0280e1278319d0eda036a6a7c19f0e1614de
Joe Weiss <joe.weiss@gmail.com>
2024-04-18 11:26:58 -0400

    Handle location = None

--- remote.py
+++ remote.py
@@ -274,17 +274,13 @@ class Remote:
         else:
             self.analyzer = self._analyzers[self.analyzer_config_key]
 
-        print(data["audio"])
-        lat = (
-            data["audio"]["location"].get("latitude", None)
-            if "location" in data["audio"]
-            else None
-        )
-        lon = (
-            data["audio"]["location"].get("longitude", None)
-            if "location" in data["audio"]
-            else None
-        )
+        if data["audio"]["location"]:
+            lat = data["audio"]["location"].get("latitude", None)
+            lon = data["audio"]["location"].get("longitude", None)
+        else:
+            lat = None
+            lon = None
+
         captured_local_date = data["audio"].get("captured_local_date", None)
         if lat and lon and captured_local_date:
             self.recording = Recording(
